f43080a41729609952719e33800f94817870d4cb
권영훈(Younghoon Kwon)/Finding개발2팀/11ST <hbdkwon@sk.com>
2019-07-18 22:10:33 +0900

    Create Project

---
+++ src/main/java/com/hbdkwon/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.hbdkwon.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Kwon!";
+    }
+}
