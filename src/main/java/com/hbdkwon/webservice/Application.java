892a5b8d0385825e1a9c70d0baaee11787d2b93b
권영훈(Younghoon Kwon)/Finding개발2팀/11ST <hbdkwon@sk.com>
2019-07-20 21:37:42 +0900

    Complete nonstop deploy!

--- src/main/java/com/hbdkwon/webservice/Application.java
+++ src/main/java/com/hbdkwon/webservice/Application.java
@@ -10,9 +10,10 @@ public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-            + "/app/config/springboot-jpa/real-application.yml";
+            + "/home/ec2-user/app/config/springboot-jpa/real-application.yml";
 
     public static void main(String[] args) {
+
         new SpringApplicationBuilder(Application.class)
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
